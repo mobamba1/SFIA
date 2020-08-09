90d29abd17adc8957f19a1c0369edeba835085ee
John Dela Cruz <47470111+mobamba1@users.noreply.github.com>
2020-08-09 20:21:16 +0100

    comments included

--- application/forms.py
+++ application/forms.py
@@ -1,12 +1,12 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length
+from flask_wtf import FlaskForm #flask forms so that it can be included in the front end
+from wtforms import StringField, SubmitField #this will used to identify the type of data being placed
+from wtforms.validators import DataRequired, Length #ensure that validators are used correctly so that no data is left out 
 
-class AddForm(FlaskForm):
+class AddForm(FlaskForm): # this form is to add new burnt and intake calories by the user
     burnt = StringField('Burnt Calories',
         validators = [
-            DataRequired(),
-            Length(min=2, max=30)
+            DataRequired(),#data needs to be inputed through the forms
+            Length(min=2, max=30)#data has to be min 2 length and 30 max
         ]
     )
     intake = StringField('Caloric Intake',
@@ -17,7 +17,7 @@ class AddForm(FlaskForm):
     )
     submit = SubmitField('Post!')
 
-class UpdateAddForm(FlaskForm):
+class UpdateAddForm(FlaskForm): # this is to update the data in SQL
     burnt = StringField('Update Burnt Calories',
         validators = [
             DataRequired(),
